4cf0225e51501d73336934b6fb0dce1181cc2978
Stevie Clifton <stevie@slowbicycle.com>
2009-02-13 15:22:57 -0500

    new updates to compression test code

--- run.rb
+++ run.rb
@@ -5,7 +5,7 @@
 # me: dia/hex/umh
 # subq: 5/6/7 (def 6)
 
-require 'compression_test'
+
 
 # test_options = [  {}, #using defaults
 #                   { :bt => '500k', :b => '500k', :pass => true},
@@ -21,23 +21,77 @@ require 'compression_test'
 #                   { :me => 'umh'},                  
 #                   { :subq => 5 },
 #                   { :subq => 7 }]
-test_options = [  { :refs => 6, :bf => 16 },
-                  { :refs => 6, :bf => 16, :crf => 16},
-                  { :refs => 6, :bf => 16, :crf => 20},
-                  { :refs => 6, :bf => 16, :crf => 24}]
-                  
-                  
 
+# CompressionTest.run(  '0RlQQf02medZl5XK0pOs0w',
+#                       '/Users/stephenclifton/code/compression_tester/input/0RlQQf02medZl5XK0pOs0w/jpg/%06d.jpg',
+#                       '/Users/stephenclifton/code/compression_tester/output/0RlQQf02medZl5XK0pOs0w',
+#                       test_options
+#                       )
+#                       
 
-CompressionTest.run( 'U73dWfU0AYC0ZTdR5SKczA',
-                     '/Users/stephenclifton/code/compression_tester/input/U73dWfU0AYC0ZTdR5SKczA/jpg/%06d.jpg',
-                      '/Users/stephenclifton/code/compression_tester/output/U73dWfU0AYC0ZTdR5SKczA',
-                      test_options
+require 'compression_test'
+CT_ROOT = '/Users/stephenclifton/code/compression_tester/'
+
+def process_one(label, format, kill_old_records)
+  # setup paths
+  input_root = CT_ROOT + "input/small/#{label}"
+  input_sequence = input_root + '/*.jpg'
+  input_audio = Dir[input_root + '/*.wav'].first
+  output_root = CT_ROOT +  "output/#{label}"      
+  
+  # setup default test
+  CompressionTester.initialize_db
+  
+  # purge old records with this label
+  if kill_old_records
+    CompressionTest.delete_all("label = '#{label}'")
+    FileUtils.rm_rf(output_root)
+  end
+  
+  FileUtils.mkdir_p(output_root)
+  
+  default_filename = input_root + "/#{label}.#{format}"
+  test = CompressionTest.new
+  test.format = format
+  test.label = label
+  test.size = '%.2f' % (File.size(default_filename) / 1024.0 / 1024.0)
+  test.flags = " old mencoder settings "
+  test.save
+  FileUtils.cp(default_filename, output_root + "/#{test.filename}")
+  
+  # set different options to test
+  test_options = [  { :preset => 'mp4_old'},
+                    { :bt => '1000k', :b => '1000k'},
+                    { :bt => '500k', :b => '500k', :pass => 2},
+                    { :bt => '1000k', :b => '1000k', :pass => 2},
+                    { :crf => 12},
+                    { :crf => 14},                    
+                    { :preset => 'iphone'},
+                    { :preset => 'iphone', :crf => 12},                    
+                    { :preset => 'iphone', :crf => 11},
+    ]
+  
+  CompressionTester.run( label, [input_sequence, input_audio], output_root, format, test_options )
+  CompressionTester.create_html( label, output_root)
+end
+
+small_vids = %w(
+1zTZLtjTF4Ml0OEMqTakwQ
+91pd5FC07QPbN2V0qtfRjA
+M6cPfqadYrkvX512DMiwag
+QNwMUrvpJsHpnZgGFPK9WA
+jqn050bhWIApvrVeCuXASQ
+yHP1pRl0HWYoLJaREzxfIA
+6vG9SQ66um1l81R0HCHeyQ
+IS5R7OkjA0QSFtuQHCUcJg
+OLUrS1Sto1hMob5VbsfcmQ
+WbI35ONGJVDutIijJrUitw
+p5oM1MYLE270fk0sN5Ml4Q
 )
 
-CompressionTest.run(  '0RlQQf02medZl5XK0pOs0w',
-                      '/Users/stephenclifton/code/compression_tester/input/0RlQQf02medZl5XK0pOs0w/jpg/%06d.jpg',
-                      '/Users/stephenclifton/code/compression_tester/output/0RlQQf02medZl5XK0pOs0w',
-                      test_options
-                      )
-                      
+#small_vids.each{|o| process_one(o) }
+
+large_vids = %w(
+0RlQQf02medZl5XK0pOs0w
+U73dWfU0AYC0ZTdR5SKczA
+)
